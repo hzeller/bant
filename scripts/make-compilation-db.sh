9b225d73b0e7542a42209853b052c9bb9ac9a572
Henner Zeller <h.zeller@acm.org>
2024-03-01 12:39:34 -0800

    Switch compile commands extraction to hedron_compile_commands
    The other repo is not maintained anymore and this seems to be in
    active development.

--- scripts/make-compilation-db.sh
+++ scripts/make-compilation-db.sh
@@ -1,15 +1,2 @@
 #!/usr/bin/env bash
-# Note, this also requires python.
-set -u
-set -e
-
-readonly OUTPUT_BASE="$(bazel info output_base)"
-
-readonly COMPDB_SCRIPT="${OUTPUT_BASE}/external/rules_compdb/generate.py"
-[ -r "${COMPDB_SCRIPT}" ] || bazel fetch ...
-
-python3 "${COMPDB_SCRIPT}"
-
-# Remove a flags observed in the wild that clang-tidy doesn't understand.
-sed -i -e 's/-fno-canonical-system-headers//g; s/DEBUG_PREFIX_MAP_PWD=.//g' \
-       compile_commands.json
+bazel run @hedron_compile_commands//:refresh_all
