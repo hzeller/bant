4450a281af1e7db48763a7d944c0ab97b68adf55
Henner Zeller <h.zeller@acm.org>
2024-05-16 18:36:16 -0700

    Be explicit what github CI image to run.
    ubuntu-latest does not have gcc-13, while ubuntu-24.04 does.

--- scripts/run-format.sh
+++ scripts/run-format.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-clang-format -i $(find bant/ -name "*.h" -o -name "*.cc")
+CLANG_FORMAT="${CLANG_FORMAT:-clang-format}"
+"${CLANG_FORMAT}" -i $(find bant/ -name "*.h" -o -name "*.cc")
 buildifier -lint=fix WORKSPACE.bzlmod MODULE.bazel $(find . -name BUILD)
