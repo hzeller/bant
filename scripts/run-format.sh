9c3dfd96ff2e9439ab960600ddb5f96ecbd5dfbb
Henner Zeller <h.zeller@acm.org>
2024-05-25 11:40:26 -0700

    WORKSPACE.bzlmod not needed anymore.

--- scripts/run-format.sh
+++ scripts/run-format.sh
@@ -2,4 +2,4 @@
 
 CLANG_FORMAT="${CLANG_FORMAT:-clang-format}"
 "${CLANG_FORMAT}" -i $(find bant/ -name "*.h" -o -name "*.cc")
-buildifier -lint=fix WORKSPACE.bzlmod MODULE.bazel $(find . -name BUILD)
+buildifier -lint=fix MODULE.bazel $(find . -name BUILD)
