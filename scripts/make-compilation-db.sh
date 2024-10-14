d34c56d75cc5c7391d9c869ac11987484fa008dd
Henner Zeller <h.zeller@acm.org>
2024-10-13 17:21:20 -0700

    If development bant can't be compiled, don't empty out compile_flags.txt

--- scripts/make-compilation-db.sh
+++ scripts/make-compilation-db.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 BAZEL=${BAZEL:-bazel}
-"${BAZEL}" fetch ...
-"${BAZEL}" build -c opt bant:bant > /dev/null 2>&1
+"${BAZEL}" fetch ... > /dev/null 2>&1
 
-bazel-bin/bant/bant compile-flags > compile_flags.txt
+# Only override compile flags if we're able to build bant.
+"${BAZEL}" build -c opt bant:bant > /dev/null 2>&1 \
+    && bazel-bin/bant/bant compile-flags > compile_flags.txt
