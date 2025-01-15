e9dc13eda33b673f7b2c4e5984f0cac34bb88090
Henner Zeller <h.zeller@acm.org>
2025-01-15 09:03:52 -0800

    Bake in tagged version in src release tar.

--- scripts/run-format.sh
+++ scripts/run-format.sh
@@ -2,4 +2,5 @@
 
 CLANG_FORMAT="${CLANG_FORMAT:-clang-format}"
 "${CLANG_FORMAT}" -i $(find bant/ -name "*.h" -o -name "*.cc")
-buildifier -lint=fix MODULE.bazel $(find . -name BUILD)
+# for now, no -lint=fix as that is version dependent
+buildifier MODULE.bazel $(find . -name BUILD)
