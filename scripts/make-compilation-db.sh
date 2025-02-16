801b78f6ce877c3c13033615713accc97bfc6ba4
Henner Zeller <h.zeller@acm.org>
2025-02-16 08:16:16 -0800

    Make sure to tickle all needed targets when building compilation-db.
    Just buliding a binary will not need gtest, so make sure it is always
    there when creating a compilation db 'cold'.

--- scripts/make-compilation-db.sh
+++ scripts/make-compilation-db.sh
@@ -2,6 +2,11 @@
 
 BAZEL=${BAZEL:-bazel}
 
+# Make sure all external projects are visible.
+# Note: bazel fetch or sync don't make them visible,
+# so we have to explicitly tickle a target there.
+"${BAZEL}" build -c opt @googletest//:gtest
+
 # Only override compile flags if we're able to build bant.
 "${BAZEL}" build -c opt bant:bant > /dev/null 2>&1 \
-    && bazel-bin/bant/bant compile-flags > compile_flags.txt
+    && bazel-bin/bant/bant compile-flags -o compile_flags.txt
