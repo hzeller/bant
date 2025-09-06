b57d45863e0a7dcaedb65249d2bdc214a35c150c
Henner Zeller <h.zeller@acm.org>
2025-09-06 14:30:27 +0200

    build status: use posix shell and be more robust.
    Don't bail if there is no volatile status, just create an
    empty file.

--- scripts/create-workspace-status.sh
+++ scripts/create-workspace-status.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 version_from_git() {
-    set -o pipefail
     git describe --match=v* 2>/dev/null \
         | sed 's/v\([^-]*\)-\([0-9]*\).*/\1-\2/' \
 	| sed 's/^v//'
@@ -10,6 +9,6 @@ version_from_git() {
 # Get version from git including count since last tag.
 BUILD_GIT_VERSION="$(version_from_git)"
 
-if [ ! -z "${BUILD_GIT_VERSION}" ]; then
+if [ -n "${BUILD_GIT_VERSION}" ]; then
     echo "BUILD_GIT_VERSION \"${BUILD_GIT_VERSION}\""
 fi
