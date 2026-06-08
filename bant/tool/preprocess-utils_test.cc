7fcf71afdca5e2860cbac69efb99f54a6b6a059d
Henner Zeller <h.zeller@acm.org>
2026-06-08 18:40:24 +0200

    Refine preprocess tests: one test per feature
    Previously, things were lumped together.

--- bant/tool/preprocess-utils_test.cc
+++ bant/tool/preprocess-utils_test.cc
@@ -46,22 +46,15 @@ static void PrintTo(const TaggedRange &r, std::ostream *out) {
 
 TEST(PreprocessUtils, PreprocessRangeIf_0_1) {
   constexpr std::string_view kTestContent = R"deftest(
-#if 0
-NO_TEXT  // Will be excluded due unmistaken situation, not BUILD influenced
-#endif
-#if A_NOT_DEFINED_MACRO
+#if 0   // A constant. unambiguously excluded, not showing up in result
 A_TEXT
+#else
+A_PRIME_TEXT
 #endif
 #if 1
 B_TEXT
-#endif
-#if 0
-#  define A_PRIME_DEF 0
-#else
-#  define A_PRIME_DEF 1
-#endif
-#if A_PRIME_DEF
-C_TEXT
+#else  // TODO: ideally, this should not be included as this is unambiguous
+B_PRIME_TEXT
 #endif
 )deftest";
 
@@ -69,78 +62,102 @@ C_TEXT
     DefineMap defs;
     auto ranges = ExtractActiveCCIfdefRanges(kTestContent, defs);
     using R = TaggedRange;
-    EXPECT_THAT(ranges, ElementsAre(R{"\n", true},         //
-                                    R{"A_TEXT\n", false},  //
-                                    R{"B_TEXT\n", true},   //
-                                    R{"C_TEXT\n", true}    //
+    EXPECT_THAT(ranges, ElementsAre(R{"\n", true},              //
+                                    R{"A_PRIME_TEXT\n", true},  //
+                                    R{"B_TEXT\n", true},        //
+                                    R{"B_PRIME_TEXT\n", false}  //
                                     ));
-    EXPECT_TRUE(defs.contains("A_PRIME_DEF"));
   }
 }
 
-TEST(PreprocessUtils, PreprocessRange) {
+TEST(PreprocessUtils, PreprocessRangeUnambiguousValueMacro) {
   constexpr std::string_view kTestContent = R"deftest(
+#if FOO  // unambiguously excluded as FOO=0, not showing up in result
 A_TEXT
-#ifndef FOO
-#define FOO
-#if A_DEF
-#  define A_PRIME_DEF 1
-#  undef A_DEF
 #else
-#  define A_PRIME_DEF 0
+A_PRIME_TEXT
 #endif
+#if BAR  // not defined macro, so ambiguous. Emit, but mark as not included
 B_TEXT
-#if A_PRIME_DEF
+#else
+B_PRIME_TEXT
+#endif
+#if BAZ  // unambiguously included
 C_TEXT
-#endif
-D_TEXT
-#ifdef B_DEF
-E_TEXT
-#else
-F_TEXT
-#endif
-#ifndef C_DEF
-G_TEXT
-#else
-H_TEXT
-#endif
+#else    // TODO: ideally, should not show up as it is also unambiguous
+C_PRIME_TEXT
 #endif
 )deftest";
 
-  using R = TaggedRange;
   {
     DefineMap defs;
-    defs["A_DEF"] = true;
+    defs["FOO"] = false;
+    // BAR not defined
+    defs["BAZ"] = true;
     auto ranges = ExtractActiveCCIfdefRanges(kTestContent, defs);
-    // Values that could be influenced by users -D choices (e.g. E_TEXT) are
-    // included, even if disabled. They could be relevant.
-    EXPECT_THAT(ranges, ElementsAre(R{"\nA_TEXT\n", true},  //
-                                    R{"B_TEXT\n", true},    //
-                                    R{"C_TEXT\n", true},    //
-                                    R{"D_TEXT\n", true},    //
-                                    R{"E_TEXT\n", false},   //
-                                    R{"F_TEXT\n", true},    //
-                                    R{"G_TEXT\n", true}, R{"H_TEXT\n", false}));
-    EXPECT_TRUE(defs.contains("FOO"));
-    EXPECT_TRUE(!defs.contains("A_DEF"));
-    EXPECT_TRUE(defs.contains("A_PRIME_DEF") && defs["A_PRIME_DEF"] == true);
+    using R = TaggedRange;
+    EXPECT_THAT(ranges, ElementsAre(R{"\n", true},              //
+                                    R{"A_PRIME_TEXT\n", true},  //
+                                    R{"B_TEXT\n", false},       //
+                                    R{"B_PRIME_TEXT\n", true},  //
+                                    R{"C_TEXT\n", true},        //
+                                    R{"C_PRIME_TEXT\n", false}  //
+                                    ));
   }
+}
+
+TEST(PreprocessUtils, PreprocessRangeUnambiguousExistenceMacro) {
+  constexpr std::string_view kTestContent = R"deftest(
+#ifdef FOO
+A_TEXT
+#else   // TODO: ideally, the following should not show up as also unambiguous
+A_PRIME_TEXT
+#endif
+#ifndef BAR  // not defined macro, so ambiguous.
+B_TEXT
+#else        // Ambiguous text included but with 'false'
+B_PRIME_TEXT
+#endif
+)deftest";
+
   {
     DefineMap defs;
-    defs["A_DEF"] = false;
-    defs["C_DEF"] = false;
+    defs["FOO"] = false;  // We only check for ifdef, so value doesn't matter
+    // BAR not defined
     auto ranges = ExtractActiveCCIfdefRanges(kTestContent, defs);
-    // If define outcomes are definitely known, don't include in output.
-    EXPECT_THAT(ranges, ElementsAre(R{"\nA_TEXT\n", true},  //
-                                    R{"B_TEXT\n", true},    //
-                                    // "C_TEXT\n" -- exclusion definitely known.
-                                    R{"D_TEXT\n", true},   //
-                                    R{"E_TEXT\n", false},  //
-                                    R{"F_TEXT\n", true},   //
-                                    // "G_TEXT\n" -- exclusion definitely known
-                                    R{"H_TEXT\n", true}));
-    EXPECT_TRUE(defs.contains("A_DEF"));
-    EXPECT_TRUE(defs.contains("A_PRIME_DEF") && defs["A_PRIME_DEF"] == false);
+    using R = TaggedRange;
+    EXPECT_THAT(ranges, ElementsAre(R{"\n", true},               //
+                                    R{"A_TEXT\n", true},         //
+                                    R{"A_PRIME_TEXT\n", false},  //
+                                    R{"B_TEXT\n", true},         //
+                                    R{"B_PRIME_TEXT\n", false}   //
+                                    ));
+  }
+}
+
+TEST(PreprocessUtils, PreprocessRangeIndirectDefinedInclusion) {
+  constexpr std::string_view kTestContent = R"deftest(
+#if 0
+#  define A_PRIME_DEF 0
+#else
+#  define A_PRIME_DEF 1
+#endif
+#if A_PRIME_DEF
+A_TEXT
+#else
+B_TEXT
+#endif
+)deftest";
+
+  {
+    DefineMap defs;
+    auto ranges = ExtractActiveCCIfdefRanges(kTestContent, defs);
+    using R = TaggedRange;
+    EXPECT_THAT(ranges, ElementsAre(R{"\n", true},        //
+                                    R{"A_TEXT\n", true},  //
+                                    R{"B_TEXT\n", false}  //
+                                    ));
+    EXPECT_TRUE(defs.contains("A_PRIME_DEF"));
   }
 }
 
