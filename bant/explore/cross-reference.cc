14dd7d71f2653a8eae20a09a92c7057240c7c75b
Henner Zeller <h.zeller@acm.org>
2026-07-26 16:43:45 +0200

    textual_hdrs: also consider containing linkable elements.
    textual_hdrs just lists source files, so we should create hyperlinks
    for them.

--- bant/explore/cross-reference.cc
+++ bant/explore/cross-reference.cc
@@ -58,8 +58,14 @@ std::unique_ptr<CrossReferenceMap> BuildCrossReferences(
           result->Insert(details.name, project.GetLocation(details.name));
         }
 
-        auto srcs_list = query::ExtractStringList(
-          {details.srcs_list, details.hdrs_list, details.data_list});
+        // TODO: here, we're somewhat particular in looking at some attributes
+        // if they contain targets or files. We should probably just go through
+        // all attributes and link everything that can be shown to be a target
+        // or file.
+
+        auto srcs_list =
+          query::ExtractStringList({details.srcs_list, details.hdrs_list,
+                                    details.data_list, details.textual_hdrs});
         for (const std::string_view src : srcs_list) {
           auto fqn =
             current_package.FullyQualifiedFile(project.workspace(), src);
